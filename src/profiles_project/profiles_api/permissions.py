46b5298b332f8b148ae0ec09ab51e71bdd94d9bb
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2018-08-26 22:52:13 +0300

    Feed permissions added

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -2,9 +2,16 @@ from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow users to edit their own profile"""
-
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit his own profile"""
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow user to update own status"""
+    def has_object_permission(self, request, view, obj):
+        """Check user is trying to update his own status"""
+        if request.method in  permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
