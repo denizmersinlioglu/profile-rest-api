855cd20e666358b8a7c87d3d52edc1c5f265a7c0
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2018-08-26 18:29:03 +0300

    Permissions and auth added to userprofileViewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from . import  serializers
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+from . import  serializers
 from . import  models
+from . import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -89,5 +93,6 @@ class HelloViewSet(viewsets.ViewSet):
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handes creating, deleting and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
-
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
