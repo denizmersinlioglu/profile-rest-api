0f6fab4c39160104776a32b8866eb57b9ba699bd
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2018-08-26 17:11:53 +0300

    Created first viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import  serializers
 from rest_framework import status
+from rest_framework import viewsets
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -42,3 +43,14 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Deletes an object"""
         return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions (list, create ,retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+        return  Response({'message:': 'Hello!', "a_viewser:": a_viewset})
